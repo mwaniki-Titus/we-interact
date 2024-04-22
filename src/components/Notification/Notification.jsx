181d6ec2c985c332c6f71e07de575b7c17a5e2f5
mwaniki-Titus <mwanikititus1@gmail.com>
2024-04-22 13:47:55 -0700

    added changes on notification

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,29 +1,33 @@
 import React, { useState } from 'react';
 import xicon from '../../assets/close.close.png';
 import './Notification.scss';
-import { useGetNotificationQuery } from '../../features/notifications/notificationApi';
+import { useGetNotificationQuery} from '../../features/notifications/notificationApi';
 import NotificationItem from './NotificationItem';
 
-const Notification = ({ onClick }) => {
-  const loggedInUserNotification = JSON.parse(localStorage.getItem('loggedInUser'));
-  const UserID = loggedInUserNotification.user.UserID;
-  const { data: Notifications } = useGetNotificationQuery(UserID);
-  const notifications = Notifications || [];
-  const [closeNotifications, setCloseNotifications] = useState(false);
 
-  const handleNotificationsClick = () => {
-    console.log('Close button clicked'); // Debugging
-    setCloseNotifications(true);
+const Notification = ({onClick}) => {
+const loggedInUserNotification=JSON.parse(localStorage.getItem('loggedInUser'))
+console.log(loggedInUserNotification);
+
+const UserID=loggedInUserNotification.user.UserID
+  const {data:Notifications}=useGetNotificationQuery(UserID)
+  // console.log(Notifications);
+  const notifications = Notifications || [];
+
+
+  console.log(notifications);
+  const [closeNotifications, setCloseNotifications] = useState(false);
+      
+      const handleNotificationsClick = () => {
+        setCloseNotifications(true);
   };
 
-  console.log('Close notifications:', closeNotifications); // Debugging
-
   return (
-    <div className={`bigmain ${closeNotifications ? 'hide' : ''}`}>
+    <div className="bigmain">
       <div className="notification">
         <div className="topNotification">
           <h4>Notification</h4>
-          <button onClick={handleNotificationsClick}>
+          <button onClick={onClick}>
             <img src={xicon} alt="Close Icon" />
           </button>
         </div>
@@ -35,12 +39,13 @@ const Notification = ({ onClick }) => {
           <h4>Today</h4>
         </div>
         <div className='TodayNotification'>
-          <div className="people">
+        <div className="people">
             {notifications.map((notification, index) => (
               <NotificationItem key={index} notification={notification} />
             ))}
           </div>
-        </div>
+          </div>
+        
       </div>
     </div>
   );
